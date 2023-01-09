ef8c443aef94df6e363a4d2ee108a90efbf01f77
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2023-01-09 17:36:56 +0100

    componente header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import "./global.css";
 import { Post } from "./Post";
-import "./global.css"
+import { Header } from "./components/Header";
 export function App() {
   return (
     <>
+    <Header />
       <Post
         author="Domingos Canhanga"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae debitis facere hic labore vitae maiores, ab nemo assumenda repellat quod, possimus esse nihil ea, magnam quas molestias quae cum sed?"
