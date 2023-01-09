4244b336d2290cb14fc4160223f438dac4311bf0
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2023-01-09 16:40:18 +0100

    css global

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <h1>Hello word</h1>
+import { Post } from "./Post";
+import "./global.css"
+export function App() {
+  return (
+    <>
+      <Post
+        author="Domingos Canhanga"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae debitis facere hic labore vitae maiores, ab nemo assumenda repellat quod, possimus esse nihil ea, magnam quas molestias quae cum sed?"
+      />
+      <Post
+        author="Arsenio Muta"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae debitis facere hic labore vitae maiores, ab nemo assumenda repellat quod, possimus esse nihil ea, magnam quas molestias quae cum sed?"
+      />
+  </>
+  );
 }
-
-export default App
