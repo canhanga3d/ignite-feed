7322d9765c89c59533da630ee760e80b0c9a67de
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2023-01-11 14:32:52 +0100

    Estilizando o sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./global.css";
+import styles from "./App.module.css";
 import { Post } from "./Post";
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
 export function App() {
   return (
     <>
     <Header />
+    <div className={styles.wrapper}>
+     <Sidebar />
+      <main>
       <Post
         author="Domingos Canhanga"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae debitis facere hic labore vitae maiores, ab nemo assumenda repellat quod, possimus esse nihil ea, magnam quas molestias quae cum sed?"
@@ -13,6 +19,9 @@ export function App() {
         author="Arsenio Muta"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae debitis facere hic labore vitae maiores, ab nemo assumenda repellat quod, possimus esse nihil ea, magnam quas molestias quae cum sed?"
       />
+      </main>
+    </div>
+      
   </>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import { PencilLine} from "phosphor-react";
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1627398242454-45a1465c2479?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+        alt=""
+      />
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/domingoscanhanga.png"
+          alt=""
+        />
+        <strong>Domingos Canhanga</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        
+        <a href="#">
+        <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
